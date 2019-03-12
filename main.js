0fae6c0941616cdb719315efd03e72bf63f51706
CODE <m_dijan@hotmail.com>
2019-03-12 19:21:05 +0100

    kraj

--- main.js
+++ main.js
@@ -1,8 +1,60 @@
 const prikaz = document.getElementById('prikaz')
 const kriterijum = document.getElementById('kriterijum')
 
+const godinaUp = document.getElementById('godina-gore')
+const godinaDown = document.getElementById('godina-dole')
+const naslovUp = document.getElementById('naslov-gore')
+const naslovDown = document.getElementById('naslov-dole')
+
+
 let sviFilmovi = []
 let rezultati = []
+
+function uporediGG(a, b) {
+  if (a.godina < b.godina)
+    return -1;
+  if (a.godina > b.godina)
+    return 1;
+  return 0;
+}
+
+function uporediGD(b, a) {
+  if (a.godina < b.godina)
+    return -1;
+  if (a.godina > b.godina)
+    return 1;
+  return 0;
+}
+
+function uporediNG(a, b) {
+  if (a.naziv < b.naziv)
+    return -1;
+  if (a.naziv > b.naziv)
+    return 1;
+  return 0;
+}
+
+function uporediND(b, a) {
+  if (a.naziv < b.naziv)
+    return -1;
+  if (a.naziv > b.naziv)
+    return 1;
+  return 0;
+}
+
+function prikazi(rezultati) {
+  stringUpis = ""
+  const limit = rezultati.length >= 14 ? 14 : rezultati.length
+  for (let i = 0; i < limit; i++) {
+    stringUpis += ` <div class= "filmski-div">
+    <h3 class= "naslov-filma">${rezultati[i].naziv}</h3> 
+    <p> Godina : ${rezultati[i].godina}</p> 
+     <img src=${rezultati[i].slika} alt="" class="slike">
+        </div> `
+  }
+  prikaz.innerHTML = stringUpis
+}
+
 
 function render(niz) {
   let sablon = ''
@@ -30,4 +82,25 @@ fetch('https://baza-filmova.herokuapp.com/filmovi/ ')
   rezultati = sviFilmovi.filter(film => film.naziv.includes(kriterijum.value))
 
   render(rezultati)
+})
+
+
+godinaUp.addEventListener("click", function() {
+  rezultati.sort(uporediGG)
+  prikazi(rezultati)
+})
+
+godinaDown.addEventListener("click", function() {
+  rezultati.sort(uporediGD)
+  prikazi(rezultati)
+})
+
+naslovUp.addEventListener("click", function () {
+  rezultati.sort(uporediNG)
+  prikazi(rezultati)
+})
+
+naslovDown.addEventListener("click", function () {
+  rezultati.sort(uporediND)
+  prikazi(rezultati)
 })
